e809b0d0dfaca9b5c755e281aef1a5ab801caf8e
Vivian Mikku Lepcha <vivian@vivians-macbook-pro-3.home>
2023-10-02 23:13:09 +0200

    Committing with Final Changes

--- pages/_document.js
+++ pages/_document.js
@@ -7,7 +7,7 @@ export default class MyDocument extends Document {
         <Head>
         <meta property="og:title" content="Ivaan Shynon Lepcha"></meta>
         <meta property="og:type" content="website"></meta>
-        <meta property="og:image" content="https://ivaanbirthday.vercel.app/images/slider/_Z060213.jpg"></meta>
+        <meta property="og:image" content={`https://ivaanbirthday.vercel.app/images/slider/_Z060213.jpg`}></meta>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link href="https://fonts.googleapis.com/css2?family=Jost:wght@300;400;500;600;700;800;900&display=swap" rel="stylesheet" />
